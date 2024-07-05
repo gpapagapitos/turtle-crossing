7fd7117c7cccc448f4c140443f57410f5c52d966
gpapagapitos <george.papagapitos@slalom.com>
2024-07-05 09:07:12 -0500

    create cars and move across screen

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
+car_manager = CarManager()
+
 screen.listen()
 screen.onkey(player.move_up, "Up")
 
@@ -18,3 +20,6 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
