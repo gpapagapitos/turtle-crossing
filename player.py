8e73468317a55350086de579197a4ecf00f7a57d
gpapagapitos <george.papagapitos@slalom.com>
2024-07-05 09:19:19 -0500

    detecting finish

--- player.py
+++ player.py
@@ -9,13 +9,25 @@ FINISH_LINE_Y = 280
 
 class Player(Turtle):
     """Class representing the player"""
+
     def __init__(self):
         super().__init__()
         self.shape("turtle")
         self.penup()
-        self.goto(STARTING_POSITION)
+        self.go_to_start()
         self.setheading(90)
 
     def move_up(self):
         """Method to move the player up"""
         self.forward(MOVE_DISTANCE)
+
+    def go_to_start(self):
+        """Method to reset the player to the starting position"""
+        self.goto(STARTING_POSITION)
+
+    def is_at_finish_line(self):
+        """Method to detect when the player is at the finish line"""
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
